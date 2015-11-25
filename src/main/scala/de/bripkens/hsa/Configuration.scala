714e7b5bf5db72357dcccdb5870db46811125991
Ben Ripkens <bripkens.dev@gmail.com>
2015-11-25 19:37:41 +0100

    Prepare reporting infrastructure

--- src/main/scala/de/bripkens/hsa/Configuration.scala
+++ src/main/scala/de/bripkens/hsa/Configuration.scala
@@ -1,12 +1,33 @@
 package de.bripkens.hsa
 
-import com.fasterxml.jackson.annotation.{JsonProperty, JsonCreator}
+import com.fasterxml.jackson.annotation.{JsonSubTypes, JsonTypeInfo, JsonProperty, JsonCreator}
 
 @JsonCreator
-case class Configuration(@JsonProperty("endpoints") endpoints: List[HealthCheckEndpoint])
+case class Configuration(@JsonProperty("endpoints") endpoints: Set[HealthCheckEndpoint],
+                         @JsonProperty("reporters") reporters: Map[String, AbstractReporter])
 
 @JsonCreator
 case class HealthCheckEndpoint(@JsonProperty("url") url: String,
                                @JsonProperty("id") id: String,
                                @JsonProperty("name") name: String,
-                               @JsonProperty("interval") interval: Int)
+                               @JsonProperty("interval") interval: Int,
+                               @JsonProperty("reporter") reporter: String)
+
+@JsonCreator
+@JsonTypeInfo(
+  use = JsonTypeInfo.Id.NAME,
+  include = JsonTypeInfo.As.PROPERTY,
+  property = "type"
+)
+@JsonSubTypes(value = Array(
+  new JsonSubTypes.Type(value = classOf[SlackReporter], name = "slack")
+))
+abstract class AbstractReporter(@JsonProperty("type") reporterType: String)
+
+case class SlackReporter(@JsonProperty("type") reporterType: String,
+                         @JsonProperty("channel") channel: String,
+                         @JsonProperty("token") token: String,
+                         @JsonProperty("botName") botName: String,
+                         @JsonProperty("botImage") botImage: String)
+    extends AbstractReporter(reporterType) {
+}
