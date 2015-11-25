17cf58233139cfb4611dca0fd6e89900a6b92e98
Ben Ripkens <bripkens.dev@gmail.com>
2015-11-25 18:25:04 +0100

    Actually execute the checks

--- src/main/scala/de/bripkens/hsa/HealthCheckActor.scala
+++ src/main/scala/de/bripkens/hsa/HealthCheckActor.scala
@@ -3,6 +3,9 @@ package de.bripkens.hsa
 import java.util.concurrent.TimeUnit
 
 import akka.actor.{ActorLogging, Actor}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{StatusCodes, HttpResponse, HttpRequest}
+import akka.stream.scaladsl.ImplicitMaterializer
 import com.fasterxml.jackson.databind.ObjectMapper
 
 import scala.concurrent.duration.Duration
@@ -10,9 +13,16 @@ import scala.concurrent.duration.Duration
 class HealthCheckActor(val mapper: ObjectMapper,
                        val config: Configuration,
                        val endpoint: HealthCheckEndpoint) extends Actor
-                                                          with ActorLogging {
+                                                          with ActorLogging
+                                                          with ImplicitMaterializer {
 
   import context.dispatcher
+
+  // make the pipeTo method available (requires the ExecutionContextExecutor)
+  // on Future
+  import akka.pattern.pipe
+
+  private val http = Http(context.system)
 
   log.info(s"Scheduling health checks for ${endpoint.url} every ${endpoint.interval} milliseconds")
   context.system.scheduler.schedule(
@@ -23,6 +33,9 @@ class HealthCheckActor(val mapper: ObjectMapper,
   )
 
   override def receive: Receive = {
+    case "check" => http.singleRequest(HttpRequest(uri = endpoint.url)).pipeTo(self)
+    case HttpResponse(StatusCodes.OK, _, _, _) => println(s"Component ${endpoint.url} is okay.")
+    case response: HttpResponse => log.info(s"Component ${endpoint.url} is having problems.")
     case unsupported => log.error(s"Unsupported message received: $unsupported")
   }
 }
