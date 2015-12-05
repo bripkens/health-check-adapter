665ba8bec15d7dc54e3ef4cad7a59bb3f3ed67c6
Ben Ripkens <bripkens.dev@gmail.com>
2015-12-05 13:18:17 +0100

    Only report status change once

--- src/main/scala/de/bripkens/hsa/reporting/ConsoleReporter.scala
+++ src/main/scala/de/bripkens/hsa/reporting/ConsoleReporter.scala
@@ -3,7 +3,7 @@ package de.bripkens.hsa.reporting
 import akka.actor.{ActorLogging, Actor}
 import com.fasterxml.jackson.databind.ObjectMapper
 import de.bripkens.hsa.ComponentStatus.ComponentStatus
-import de.bripkens.hsa.{ComponentStatus, ComponentStatusUpdate, ConsoleReporterConfig}
+import de.bripkens.hsa.{HealthCheckEndpoint, ComponentStatus, ComponentStatusUpdate, ConsoleReporterConfig}
 
 import scala.collection.mutable
 
@@ -14,17 +14,24 @@ class ConsoleReporter(val mapper: ObjectMapper, val config: ConsoleReporterConfi
 
   override def receive: Receive = {
     case ComponentStatusUpdate(component, ComponentStatus.OKAY, _) => {
-      if (componentStatus.get(component.id) != ComponentStatus.OKAY) {
+      if (!componentStatus.get(component.id).contains(ComponentStatus.OKAY)) {
         log.info(s"${component.name} is okay.")
         componentStatus.put(component.id, ComponentStatus.OKAY)
       }
     }
     case ComponentStatusUpdate(component, ComponentStatus.UNHEALTHY, _) => {
-      log.info(s"${component.name} has some issues.")
+      if (!componentStatus.get(component.id).contains(ComponentStatus.UNHEALTHY)) {
+        log.info(s"${component.name} has some issues.")
+        componentStatus.put(component.id, ComponentStatus.UNHEALTHY)
+      }
     }
     case ComponentStatusUpdate(component, ComponentStatus.NOT_REACHABLE, _) => {
-      log.info(s"${component.name} cannot be reached.")
+      if (!componentStatus.get(component.id).contains(ComponentStatus.NOT_REACHABLE)) {
+        log.info(s"${component.name} cannot be reached.")
+        componentStatus.put(component.id, ComponentStatus.NOT_REACHABLE)
+      }
     }
     case unsupported => log.error(s"Unsupported message received: $unsupported")
   }
+
 }
